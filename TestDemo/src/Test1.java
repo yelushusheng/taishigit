fcd65aa0826b062fc5d388018443db89cfa67d20
yelushusheng <13380785787@163.com>
2020-12-07 01:06:35 +0800

    新一次提交

---
+++ TestDemo/src/Test1.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+/**
+ * @Author: 夜路书生
+ * @Description：
+ * @Time： 2020/12/7 0:29
+ * @Email： 13380785787@163.com
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+    @Test
+    public void test() throws Exception{
+        System.out.println("22222222222");
+    }
+}
+
